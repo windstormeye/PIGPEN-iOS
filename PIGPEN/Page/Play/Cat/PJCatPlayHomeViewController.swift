d512f8d0eafdd20590dac0b9f4fffd3c77e69e8f
pjhubs <877302410@qq.com>
2019-06-15 23:44:25 +0800

    [v0.0.1] <release> cat play UI

--- PIGPEN/Page/Play/Cat/PJCatPlayHomeViewController.swift
+++ PIGPEN/Page/Play/Cat/PJCatPlayHomeViewController.swift
@@ -13,6 +13,7 @@ class PJCatPlayHomeViewController: UIViewController, PJBaseViewControllerDelegat
     var viewModels = [PJPet.Pet]()
     
     private var avatarView = PJPetAvatarView()
+    private var bottomView = PJBottomDotButtonView()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -38,52 +39,27 @@ class PJCatPlayHomeViewController: UIViewController, PJBaseViewControllerDelegat
         avatarView.scrollToButton(0)
         view.addSubview(avatarView)
         
-        // 撸猫动图
-        let activityImageView = UIImageView(frame: CGRect(x: 0, y: avatarView.bottom + 40, width: view.pj_width * 0.53, height: view.pj_width * 0.53 * 1.124))
-        activityImageView.centerX = view.centerX
-        activityImageView.loadGif(asset: "timg")
-        view.addSubview(activityImageView)
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: navigationBarHeight, width: view.pj_width, height: view.pj_height - navigationBarHeight))
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.delegate = self
+        view.addSubview(scrollView)
+        view.sendSubviewToBack(scrollView)
         
-        let timeMsgView = UIView(frame: CGRect(x: 15, y: activityImageView.bottom + 30, width: view.pj_width - 30, height: 80))
-        timeMsgView.layer.cornerRadius = timeMsgView.pj_height / 2
-        let msgDetailsView = PJPetPlayDetailsView.newInstance()
-        msgDetailsView.pj_width = timeMsgView.pj_width
-        msgDetailsView.y = (timeMsgView.pj_height - msgDetailsView.pj_height) / 2
-        timeMsgView.backgroundColor = msgDetailsView.backgroundColor
+        for index in 0..<viewModels.count {
+            let detailsView = PJCatPlayDetailsView(frame: CGRect(x: CGFloat(index) * view.pj_width, y: 0, width: view.pj_width, height: scrollView.pj_height))
+            scrollView.addSubview(detailsView)
+            
+            scrollView.contentSize = CGSize(width: detailsView.right, height: 0)
+        }
         
-        timeMsgView.addSubview(msgDetailsView)
-        view.addSubview(timeMsgView)
         
-        let editButton = UIButton(frame: CGRect(x: 0, y: timeMsgView.bottom + 30, width: 120, height: 36))
-        view.addSubview(editButton)
-        editButton.centerX = view.centerX
-        editButton.setTitle("修改记录", for: .normal)
-        editButton.setImage(UIImage(named: "pet_play_edit"), for: .normal)
-        editButton.addTarget(self, action: .edit, for: .touchUpInside)
-        editButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        editButton.layer.cornerRadius = editButton.pj_height / 2
-        editButton.clipsToBounds = true
-        editButton.layer.borderColor = UIColor.black.cgColor
-        editButton.layer.borderWidth = 1.5
-        editButton.setTitleColor(.black, for: .normal)
-        
-        let addButton = UIButton(frame: CGRect(x: editButton.x, y: editButton.bottom + 20, width: editButton.pj_width, height: editButton.pj_height))
-        view.addSubview(addButton)
-        addButton.setTitle("手动添加", for: .normal)
-        addButton.titleLabel?.font = editButton.titleLabel?.font
-        addButton.setTitleColor(editButton.titleColor(for: .normal), for: .normal)
-        addButton.setImage(UIImage(named: "pet_play_add"), for: .normal)
-        addButton.addTarget(self, action: .edit, for: .touchUpInside)
-        addButton.layer.cornerRadius = editButton.layer.cornerRadius
-        addButton.clipsToBounds = true
-        addButton.layer.borderColor = editButton.layer.borderColor
-        addButton.layer.borderWidth = editButton.layer.borderWidth
-        
-        let bottomView = PJBottomDotButtonView(frame: CGRect(x: 0, y: view.pj_height - bottomSafeAreaHeight - 40, width: view.pj_width, height: 36), pageCount: viewModels.count - 1)
+        bottomView = PJBottomDotButtonView(frame: CGRect(x: 0, y: view.pj_height - bottomSafeAreaHeight - 40, width: view.pj_width, height: 36), pageCount: viewModels.count - 1)
         view.addSubview(bottomView)
         
         avatarView.itemSelected = {
-            bottomView.updateDot($0)
+            self.bottomView.updateDot($0)
+            scrollView.setContentOffset(CGPoint(x: CGFloat($0) * self.view.pj_width, y: 0), animated: true)
         }
     }
 }
@@ -109,4 +85,15 @@ private extension Selector {
     static let back = #selector(PJCatPlayHomeViewController.back)
     static let edit = #selector(PJCatPlayHomeViewController.edit)
     static let add = #selector(PJCatPlayHomeViewController.add)
+}
+
+extension PJCatPlayHomeViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetX = scrollView.contentOffset.x
+        let page = offsetX / view.pj_width
+        
+        avatarView.scrollToButton(Int(page))
+        bottomView.updateDot(Int(page))
+    }
 }
---
+++ PIGPEN/Page/Play/Cat/View/Details/PJCatPlayDetailsView.swift
@@ -0,0 +1,82 @@
+//
+//  PJCatPlayDetailsView.swift
+//  PIGPEN
+//
+//  Created by 翁培钧 on 2019/6/15.
+//  Copyright © 2019 PJHubs. All rights reserved.
+//
+
+import UIKit
+
+class PJCatPlayDetailsView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initView() {
+        // 撸猫动图
+        let activityImageView = UIImageView(frame: CGRect(x: 0, y: 36 * 1.385 + 40, width: pj_width * 0.53, height: pj_width * 0.53 * 1.124))
+        activityImageView.x = (pj_width - pj_width * 0.53) / 2
+        activityImageView.loadGif(asset: "timg")
+        addSubview(activityImageView)
+        
+        let timeMsgView = UIView(frame: CGRect(x: 15, y: activityImageView.bottom + 30, width: pj_width - 30, height: 80))
+        timeMsgView.layer.cornerRadius = timeMsgView.pj_height / 2
+        let msgDetailsView = PJPetPlayDetailsView.newInstance()
+        msgDetailsView.pj_width = timeMsgView.pj_width
+        msgDetailsView.y = (timeMsgView.pj_height - msgDetailsView.pj_height) / 2
+        timeMsgView.backgroundColor = msgDetailsView.backgroundColor
+        
+        timeMsgView.addSubview(msgDetailsView)
+        addSubview(timeMsgView)
+        
+        let editButton = UIButton(frame: CGRect(x: 0, y: timeMsgView.bottom + 30, width: 120, height: 36))
+        addSubview(editButton)
+        editButton.x = (pj_width - 120) / 2
+        editButton.setTitle("修改记录", for: .normal)
+        editButton.setImage(UIImage(named: "pet_play_edit"), for: .normal)
+        editButton.addTarget(self, action: .edit, for: .touchUpInside)
+        editButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        editButton.layer.cornerRadius = editButton.pj_height / 2
+        editButton.clipsToBounds = true
+        editButton.layer.borderColor = UIColor.black.cgColor
+        editButton.layer.borderWidth = 1.5
+        editButton.setTitleColor(.black, for: .normal)
+        
+        let addButton = UIButton(frame: CGRect(x: editButton.x, y: editButton.bottom + 20, width: editButton.pj_width, height: editButton.pj_height))
+        addSubview(addButton)
+        addButton.setTitle("手动添加", for: .normal)
+        addButton.titleLabel?.font = editButton.titleLabel?.font
+        addButton.setTitleColor(editButton.titleColor(for: .normal), for: .normal)
+        addButton.setImage(UIImage(named: "pet_play_add"), for: .normal)
+        addButton.addTarget(self, action: .edit, for: .touchUpInside)
+        addButton.layer.cornerRadius = editButton.layer.cornerRadius
+        addButton.clipsToBounds = true
+        addButton.layer.borderColor = editButton.layer.borderColor
+        addButton.layer.borderWidth = editButton.layer.borderWidth
+    }
+
+}
+
+extension PJCatPlayDetailsView {
+    @objc
+    fileprivate func edit() {
+        
+    }
+    
+    @objc
+    fileprivate func add() {
+        
+    }
+}
+
+private extension Selector {
+    static let edit = #selector(PJCatPlayDetailsView.edit)
+    static let add = #selector(PJCatPlayDetailsView.add)
+}
