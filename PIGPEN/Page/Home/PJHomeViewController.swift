4377bc1b5476d84393af039e3f9288aef86f2152
pjhubs <877302410@qq.com>
2019-07-21 16:43:38 +0800

    [v0.0.1] <add> Around Pet add load more

--- PIGPEN/Page/Home/PJHomeViewController.swift
+++ PIGPEN/Page/Home/PJHomeViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import ESPullToRefresh
+
 
 class PJHomeViewController: UIViewController, PJBaseViewControllerDelegate {
     
     private var locationManager = AMapLocationManager()
-    private var refresher = UIRefreshControl()
     private var collectionView = PIGAroundPetCollectionView()
     
+    private var refresher = UIRefreshControl()
     private var collectionBottomView = UIView()
+    
+    private var page = 1
+    
     
     private var coor = CLLocationCoordinate2D(latitude: 0, longitude: 0) {
         didSet {
-            requestData()
+            requestData(page: 1)
         }
     }
     
@@ -46,33 +51,14 @@ class PJHomeViewController: UIViewController, PJBaseViewControllerDelegate {
         collectionView = PIGAroundPetCollectionView(frame: CGRect(x: 0, y: navigationBarHeight, width: view.pj_width, height: view.pj_height - navigationBarHeight - PJTABBAR_HEIGHT), collectionViewLayout: collectionViewLayout)
         collectionView.backgroundColor = .clear
         collectionView.refreshControl = refresher
-        collectionView.delegate = self
         view.addSubview(collectionView)
         
-        collectionBottomView = UIView(frame: CGRect(x: 0, y: collectionView.bottom, width: view.pj_width, height: 50))
-        
-        let tipLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        collectionBottomView.addSubview(tipLabel)
-        collectionBottomView.isHidden = true
-        tipLabel.text = "正在加载..."
-        tipLabel.textAlignment = .center
-        tipLabel.font = UIFont.systemFont(ofSize: 14)
-        tipLabel.sizeToFit()
-        tipLabel.centerX = view.centerX
-        tipLabel.y = (tipLabel.superview!.pj_height - tipLabel.pj_height) / 2
-        tipLabel.textColor = PJRGB(200, 200, 200)
-        
-        let indicatorView = UIActivityIndicatorView(style: .gray)
-        indicatorView.pj_width = 10
-        indicatorView.pj_height = 10
-        indicatorView.right = tipLabel.left - 5
-        tipLabel.left += 15
-        collectionBottomView.addSubview(indicatorView)
-        indicatorView.y = (indicatorView.superview!.pj_height - indicatorView.pj_height) / 2
-        indicatorView.startAnimating()
-        
-        view.addSubview(collectionBottomView)
-        collectionView.contentInset.bottom = 50
+        collectionView.es.addInfiniteScrolling { [weak self] in
+            guard let `self` = self else { return }
+            
+            self.page += 1
+            self.requestData(page: self.page)
+        }
         
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.delegate = self
@@ -113,20 +99,19 @@ extension PJHomeViewController: AMapLocationManagerDelegate {
 
 
 extension PJHomeViewController {
-    private func requestData() {
+    private func requestData(page: Int) {
         PJPet.around(uid: String(PJUser.shared.userModel.uid),
                      latitude: String(coor.latitude),
                      longitude: String(coor.longitude),
-                     page: 0,
+                     page: page,
                      complationHandler: {
-                        var models = $0
-                        models += $0
-                        models += $0
-                        models += $0
-                        models += $0
-                        models += $0
-                        self.collectionView.viewModels = models
-                        self.refresher.endRefreshing()
+                        if page > 1 {
+                            self.collectionView.viewModels += $0
+                            self.collectionView.es.stopLoadingMore()
+                        } else {
+                            self.collectionView.viewModels = $0
+                            self.refresher.endRefreshing()
+                        }
         }) {
             PJHUD.shared.showError(view: self.view, text: $0.errorMsg)
             self.refresher.endRefreshing()
@@ -134,45 +119,10 @@ extension PJHomeViewController {
     }
 }
 
-extension PJHomeViewController: UIScrollViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("scrollContentOffSet: \(scrollView.contentOffset.y)")
-        print("scrollContentSize: \(scrollView.contentSize.height)")
-        
-        let type = true
-        
-        if scrollView.contentSize.height - scrollView.contentOffset.y <= scrollView.pj_height {
-            collectionBottomView.isHidden = false
-            if type {
-                let bottomViewOffSetY = scrollView.contentOffset.y - (scrollView.contentSize.height - scrollView.pj_height)
-                collectionBottomView.top = scrollView.bottom - bottomViewOffSetY
-            } else {
-                if scrollView.contentSize.height - scrollView.contentOffset.y + 50 > scrollView.pj_height {
-                    let bottomViewOffSetY = scrollView.contentOffset.y - (scrollView.contentSize.height - scrollView.pj_height)
-                    collectionBottomView.top = scrollView.bottom - bottomViewOffSetY
-                } else {
-                    collectionBottomView.top = scrollView.bottom - 50
-                }
-            }
-        } else {
-            if type {
-                let bottomViewOffSetY = scrollView.contentOffset.y - (scrollView.contentSize.height - scrollView.pj_height)
-                collectionBottomView.top = scrollView.bottom - bottomViewOffSetY
-            }
-            collectionBottomView.isHidden = true
-        }
-        
-        print("bottomViewTop: \(collectionBottomView.top)")
-    }
-}
-
-extension PJHomeViewController: UICollectionViewDelegate {}
-
 extension PJHomeViewController {
     @objc
     fileprivate func refresh() {
-        requestData()
+        requestData(page: 1)
     }
     
     @objc fileprivate func gotoLoginPage() {
