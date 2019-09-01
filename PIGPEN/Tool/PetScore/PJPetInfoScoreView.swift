a97ba2247b693bd4a2df443cfd922fec0ccf8208
pjhubs <877302410@qq.com>
2019-09-01 23:59:43 +0800

    [v0.0.1] <add> icon

---
+++ PIGPEN/Tool/PetScore/PJPetInfoScoreView.swift
@@ -0,0 +1,21 @@
+//
+//  PJPetInfoScoreView.swift
+//  PIGPEN
+//
+//  Created by 翁培钧 on 2019/9/1.
+//  Copyright © 2019 PJHubs. All rights reserved.
+//
+
+import UIKit
+
+class PJPetInfoScoreView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
